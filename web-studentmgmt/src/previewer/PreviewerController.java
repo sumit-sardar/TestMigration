b6395eeb9cd33eb1d793fb260a68eb7e3d8bf4c5
nirmalab <>
2010-02-23 13:44:06 +0000

    changes for defect 62681

--- web-studentmgmt/src/previewer/PreviewerController.java
+++ web-studentmgmt/src/previewer/PreviewerController.java
@@ -323,9 +323,9 @@ public class PreviewerController extends PageFlowController
             
         String pageTitle = "Color and Font Preview";
         
-        
-        String loginXML = "<tmssvc_response method=\"login_response\"><login_response restart_number=\"0\" restart_flag=\"false\" lsid=\"1:swirl50346\" ><status status_code=\"200\"/>" +
-         "<testing_session_data><cmi.core student_first_name=\"" + this.firstName + "\" student_last_name=\"" + this.lastName + "\"student_middle_name=\"\" student_id=\"12345\" />" +
+        //changes for defect 62681-- speedValue was missing
+        String loginXML = "<tmssvc_response method=\"login_response\"><login_response restart_number=\"0\" restart_flag=\"false\" lsid=\"1:swirl50346\" tts_speed_value=\"S\"><status status_code=\"200\"/>" +
+          "<testing_session_data><cmi.core student_first_name=\"" + this.firstName + "\" student_last_name=\"" + this.lastName + "\"student_middle_name=\"\" student_id=\"12345\" />" +
           "<lms.student.accommodations calculator=\"" + calculator + "\" magnifier=\"" + magnifier + 
           "\" screen_reader=\"" + screenReader + "\" untimed=\"" + untimed + "\" rest_break=\"" + rest_break + "\" highlighter=\"" + highlighter+ 
           "\"><stereotype_style stereotype=\"directions\" bgcolor=\"" +  questionBgColor+ "\" font_color=\"" + questionFgColor + "\" font_magnification=\"" + questionFontSize +
