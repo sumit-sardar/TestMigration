fc2d2a5c6b64cdf980986d93b1d121dda12b5165
psarmi <>
2010-11-08 20:07:01 +0000

    ISTEP2011CR023 script

--- web-studentmgmt/src/dto/Message.java
+++ web-studentmgmt/src/dto/Message.java
@@ -32,7 +32,7 @@ public class Message implements java.io.Serializable
     public static final String DELETE_ERROR_TAS = "You cannot delete this student. Student is associated with test administrations.";
     public static final String DELETE_ERROR_UNKNOWN = "Failed to delete this student.";
     
-    public static final String STUDENT_ASSIGNMENT_ERROR = "Student cannot be associated with more than one organization.";     //Added for CR  ISTEP2011CR017
+    public static final String STUDENT_ASSIGNMENT_ERROR = "Your school system does not allow students to be associated with more than one organization (class) for testing. Please choose only one.";     //Added for CR  ISTEP2011CR023
 
     public Message()
     {
