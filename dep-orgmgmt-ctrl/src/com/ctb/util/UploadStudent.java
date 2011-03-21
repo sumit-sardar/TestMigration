6334159f0a56fce865b9a27d63fe850d9538508d
ttruong <>
2011-03-21 01:08:19 +0000

    Fix for defect 62953

--- dep-orgmgmt-ctrl/src/com/ctb/util/UploadStudent.java
+++ dep-orgmgmt-ctrl/src/com/ctb/util/UploadStudent.java
@@ -519,6 +519,13 @@ public class UploadStudent extends BatchProcessor.Process
 
 		} catch (Exception e) {
 
+            dataFileAudit.setStatus("FL");
+            try{
+               uploadDataFile.upDateAuditTable(dataFileAudit);
+            } catch (SQLException ex) {
+               ex.printStackTrace();
+            }
+			
 			e.printStackTrace();    
 		}
 
