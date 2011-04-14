9be3c7824f3cf876bd58df696c3fdfee399a2c22
nirmalab <>
2011-04-14 12:17:19 +0000

    Rubric Data Retrieval for cr scoring

--- web-handscoring/src/utils/JsonUtils.java
+++ web-handscoring/src/utils/JsonUtils.java
@@ -2,6 +2,7 @@ package utils;
 
 import com.thoughtworks.xstream.XStream;
 import com.thoughtworks.xstream.io.json.JettisonMappedXmlDriver;
+import com.ctb.bean.testAdmin.RubricViewData;
 
 public class JsonUtils {
 
@@ -12,6 +13,7 @@ public static String getJson (Object _object,String alias,Class<?> _class) throw
 		XStream jsondriver = new XStream(
 				new JettisonMappedXmlDriver());
 		jsondriver.alias(alias,_class);
+		jsondriver.alias("entry",RubricViewData.class);
 		return jsondriver.toXML(_object);
 		
 	}
