a66ceb43a298a2d4f0b4c0b398ff79f65be25f1a
ncohen <>
2011-06-03 22:29:19 +0000

    *** empty log message ***

--- application-tmsproxy/remoteservlet/src/com/ctb/tms/web/servlet/TMSServlet.java
+++ application-tmsproxy/remoteservlet/src/com/ctb/tms/web/servlet/TMSServlet.java
@@ -20,53 +20,22 @@ public class TMSServlet extends HttpServlet {
 
 	private static final long serialVersionUID = 1L;
 
-	/**
-	 * Constructor of the object.
-	 */
 	public TMSServlet() {
 		super();
 	}
 
-    /**
-     * Initialization of the servlet. <br>
-     *
-     * @throws ServletException if an error occure
-     */
     public void init() throws ServletException {
         // do nothing
     }
-        
-	/**
-	 * Destruction of the servlet. <br>
-	 */
+
 	public void destroy() {
-		super.destroy(); // Just puts "destroy" string in log
+		super.destroy(); 
 	}
 
-    /**
-     * The doPost method of the servlet. <br>
-     *
-     * This method is called when a form has its tag value method equals to post.
-     * 
-     * @param request the request send by the client to the server
-     * @param response the response send by the server to the client
-     * @throws ServletException if an error occurred
-     * @throws IOException if an error occurred
-     */
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {    
         doGet(request, response);            
     }
 
-	/**
-	 * The doGet method of the servlet. <br>
-	 *
-	 * This method is called when a form has its tag value method equals to get.
-	 * 
-	 * @param request the request send by the client to the server
-	 * @param response the response send by the server to the client
-	 * @throws ServletException if an error occurred
-	 * @throws IOException if an error occurred
-	 */
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String method = getMethod(request);
 		String result = ServletUtils.OK;
