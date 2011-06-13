7ec1296a648d9789ab055cc4ffc8c1c678c24a66
nirmalab <>
2011-06-13 12:46:41 +0000

    Script for masking ruler change and auditory calming

--- oasx/LASLINK/DDL/DDL.sql
+++ oasx/LASLINK/DDL/DDL.sql
@@ -13,3 +13,22 @@ ALTER TABLE student_item_set_status ADD absent VARCHAR2(1)
 
 ALTER TABLE org_node ADD org_node_mdr_number VARCHAR2(32)
 /
+
+
+/* New Table was created for keeping the list of music files in database */
+
+CREATE TABLE Music_File_List(
+file_id INTEGER PRIMARY KEY,
+audio_file_name VARCHAR2(100),
+audio_file BLOB 
+);
+/
+
+
+
+/* Two new columns music_file_id and masking_ruler were added in table student_accomodation */
+
+ALTER TABLE student_accomodation ADD music_file_id VARCHAR2(200)
+/
+ALTER TABLE student_accomodation ADD masking_ruler VARCHAR2(2)
+/
