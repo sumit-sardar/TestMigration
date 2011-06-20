7606f360838dd07ce65c2561f5917ada2d9c6e6c
nirmalab <>
2011-06-20 15:16:41 +0000

    change script for laslink and llespanol

--- oasx/LASLINK/STORED PROCEDURE/PROCEDURE/setup_llespanol_customer.sql
+++ oasx/LASLINK/STORED PROCEDURE/PROCEDURE/setup_llespanol_customer.sql
@@ -245,7 +245,7 @@ Insert into PROGRAM
    (CUSTOMER_ID, PRODUCT_ID, PROGRAM_ID, PROGRAM_NAME, PROGRAM_START_DATE, PROGRAM_END_DATE, NORMS_GROUP, NORMS_YEAR, ACTIVATION_STATUS, CREATED_DATE_TIME, UPDATED_DATE_TIME)
  Values
    (cust_id, 7000, cust_id, (select customer_name from customer where customer_id = cust_id) || ' Program', 
-    sysdate, sysdate + 365, '19', '2011', 'AC', 
+    sysdate, sysdate + 365, NULL , '2011', 'AC', 
     sysdate, sysdate); 
     
 
