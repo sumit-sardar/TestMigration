0d8795f77fcf1fcfbe94fec3ad61a9c6fb587f90
nirmalab <>
2011-06-21 15:38:00 +0000

    DML Queries for IRS Database

---
+++ oasx/LASLINK/DML/DML_IRS.sql
@@ -0,0 +1,295 @@
+--form_dim
+insert into form_dim values (7, 'A', 4)
+/
+insert into form_dim values (8, 'B', 4)
+/
+insert into form_dim values (9, 'Español', 4)
+/
+insert into form_dim values (10, 'N/A', 4)
+/
+
+
+--level_dim
+insert into level_dim values (16,'K',4)
+/
+insert into level_dim values (17,'1',4)
+/
+insert into level_dim values (18,'2-3',4)
+/
+insert into level_dim values (19,'4-5',4)
+/
+insert into level_dim values (20,'6-8',4)
+/
+insert into level_dim values (21,'9-12',4)
+/
+insert into level_dim values (22,'N/A',4)
+
+
+
+--grade_dim
+insert into grade_dim values (14,'K',4)
+/
+insert into grade_dim values (15,'1',4)
+/
+insert into grade_dim values (16,'2',4)
+/
+insert into grade_dim values (17,'3',4)
+/
+insert into grade_dim values (18,'4',4)
+/
+insert into grade_dim values (19,'5',4)
+/
+insert into grade_dim values (20,'6',4)
+/
+insert into grade_dim values (21,'7',4)
+/
+insert into grade_dim values (22,'8',4)
+/
+insert into grade_dim values (23,'9',4)
+/
+insert into grade_dim values (24,'10',4)
+/
+insert into grade_dim values (25,'11',4)
+/
+insert into grade_dim values (26,'12',4)
+/
+insert into grade_dim values (27,'N/A',4)
+/
+
+
+
+--ATTR18_DIM 
+insert into ATTR18_DIM (attr18id, NAME, TYPE, product_typeId) 
+    values (1, 'K', 'MOBILITY', 4)
+/
+insert into ATTR18_DIM (attr18id, NAME, TYPE, product_typeId) 
+    values (2, '1', 'MOBILITY', 4)
+/
+insert into ATTR18_DIM (attr18id, NAME, TYPE, product_typeId) 
+    values (3, '2', 'MOBILITY', 4)
+/
+insert into ATTR18_DIM (attr18id, NAME, TYPE, product_typeId) 
+    values (4, '3', 'MOBILITY', 4)
+/
+insert into ATTR18_DIM (attr18id, NAME, TYPE, product_typeId) 
+    values (5, '4', 'MOBILITY', 4)
+/
+insert into ATTR18_DIM (attr18id, NAME, TYPE, product_typeId) 
+    values (6, '5', 'MOBILITY', 4)
+/
+insert into ATTR18_DIM (attr18id, NAME, TYPE, product_typeId) 
+    values (7, '6', 'MOBILITY', 4)
+/
+insert into ATTR18_DIM (attr18id, NAME, TYPE, product_typeId) 
+    values (8, '7', 'MOBILITY', 4)
+/
+insert into ATTR18_DIM (attr18id, NAME, TYPE, product_typeId) 
+    values (9, '8', 'MOBILITY', 4)
+/
+insert into ATTR18_DIM (attr18id, NAME, TYPE, product_typeId) 
+    values (10, '9', 'MOBILITY', 4)
+/
+insert into ATTR18_DIM (attr18id, NAME, TYPE, product_typeId) 
+    values (11, '10', 'MOBILITY', 4)
+/
+insert into ATTR18_DIM (attr18id, NAME, TYPE, product_typeId) 
+    values (12, '11', 'MOBILITY', 4)
+/
+insert into ATTR18_DIM (attr18id, NAME, TYPE, product_typeId) 
+    values (13, '12', 'MOBILITY', 4)
+/
+
+
+
+--ATTR20_DIM 
+insert into ATTR20_DIM (attr20id, NAME, TYPE, product_typeId) 
+    values (1, 'ESEA Title 1', 'PROGRAM_PARTICIPATION', 4)
+/
+insert into ATTR20_DIM (attr20id, NAME, TYPE, product_typeId) 
+    values (2, 'English Language Learner (ESEA Title III)', 	'PROGRAM_PARTICIPATION', 4)
+/
+insert into ATTR20_DIM (attr20id, NAME, TYPE, product_typeId) 
+    values (3, 'Gifted and Talented', 'PROGRAM_PARTICIPATION', 4)
+/
+insert into ATTR20_DIM (attr20id, NAME, TYPE, product_typeId) 
+    values (4, 'Indian Education', 'PROGRAM_PARTICIPATION', 4)
+/
+insert into ATTR20_DIM (attr20id, NAME, TYPE, product_typeId) 
+    values (5, 'Migrant Education', 'PROGRAM_PARTICIPATION', 4)
+/
+
+
+
+--ATTR21_DIM 
+insert into ATTR21_DIM (attr21id, NAME, TYPE, product_typeId) 
+    values (1, 'IEP', 'SPECIAL_EDUCATION', 4)
+/
+insert into ATTR21_DIM (attr21id, NAME, TYPE, product_typeId) 
+    values (2, '504', 'SPECIAL_EDUCATION', 4)
+/
+
+
+--ATTR22_DIM 
+insert into ATTR22_DIM (attr22id, NAME, TYPE, product_typeId) 
+    values (1, 'A', 'DISABILITY', 4)
+/
+insert into ATTR22_DIM (attr22id, NAME, TYPE, product_typeId) 
+    values (2, 'D', 'DISABILITY', 4)
+/
+insert into ATTR22_DIM (attr22id, NAME, TYPE, product_typeId) 
+    values (3, 'HI', 'DISABILITY', 4)
+/
+insert into ATTR22_DIM (attr22id, NAME, TYPE, product_typeId) 
+    values (4, 'MU', 'DISABILITY', 4)
+/
+insert into ATTR22_DIM (attr22id, NAME, TYPE, product_typeId) 
+    values (5, 'OI', 'DISABILITY', 4)
+/
+insert into ATTR22_DIM (attr22id, NAME, TYPE, product_typeId) 
+    values (6, 'OHI', 'DISABILITY', 4)
+/
+insert into ATTR22_DIM (attr22id, NAME, TYPE, product_typeId) 
+    values (7, 'SED', 'DISABILITY', 4)
+/
+insert into ATTR22_DIM (attr22id, NAME, TYPE, product_typeId) 
+    values (8, 'LN', 'DISABILITY', 4)
+/
+insert into ATTR22_DIM (attr22id, NAME, TYPE, product_typeId) 
+    values (9, 'SLI', 'DISABILITY', 4);
+/
+insert into ATTR22_DIM (attr22id, NAME, TYPE, product_typeId) 
+    values (10, 'TBI', 'DISABILITY', 4)
+/
+insert into ATTR22_DIM (attr22id, NAME, TYPE, product_typeId) 
+    values (11, 'VI', 'DISABILITYI', 4)
+/
+insert into ATTR22_DIM (attr22id, NAME, TYPE, product_typeId) 
+    values (12, 'ME', 'DISABILITY', 4)
+/
+
+
+
+--ATTR23_DIM 
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (1, 'DC-S', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (2, 'DC-L', 'ACCOMMODATIONS-L', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (3, 'DC-RD', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (4, 'DC-WR', 'ACCOMMODATIONS', 4)
+/
+        
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (5, 'RQE-S', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (6, 'RQE-L', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (7, 'RQE-RD', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (8, 'RQE-WR', 'ACCOMMODATIONS', 4)
+/
+    
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (9, 'RPE-S', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (10, 'RPE-L', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (11, 'RPE-RD', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (12, 'RPE-WR', 'ACCOMMODATIONS', 4)
+/
+    
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (13, 'RSR-S', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (14, 'RSR-L', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (15, 'RSR-RD', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (16, 'RSR-WR', 'ACCOMMODATIONS', 4)
+/
+    
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (17, 'SA-S', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (18, 'SA-L', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (19, 'SA-RD', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (20, 'SA-WR', 'ACCOMMODATIONS', 4)
+/
+    
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (21, 'ASM-S', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (22, 'ASM-L', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (23, 'ASM-RD', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (24, 'ASM-WR', 'ACCOMMODATIONS', 4)
+/
+    
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (25, 'RDNL-S', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (26, 'RDNL-L', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (27, 'RDNL-RD', 'ACCOMMODATIONS', 4)
+/
+insert into ATTR23_DIM (attr23id, NAME, TYPE, product_typeId) 
+    values (28, 'RDNL-WR', 'ACCOMMODATIONS', 4)
+/
+
+
+
+
+--ATTR24_DIM 
+insert into ATTR24_DIM (attr24id, NAME, TYPE, product_typeId) 
+    values (1, '0', 'SPECIAL CODES', 4)
+/
+insert into ATTR24_DIM (attr24id, NAME, TYPE, product_typeId) 
+    values (2, '1', 'SPECIAL CODES', 4)
+/
+insert into ATTR24_DIM (attr24id, NAME, TYPE, product_typeId) 
+    values (3, '2', 'SPECIAL CODES', 4)
+/
+insert into ATTR24_DIM (attr24id, NAME, TYPE, product_typeId) 
+    values (4, '3', 'SPECIAL CODES', 4)
+/
+insert into ATTR24_DIM (attr24id, NAME, TYPE, product_typeId) 
+    values (5, '4', 'SPECIAL CODES', 4)
+/
+insert into ATTR24_DIM (attr24id, NAME, TYPE, product_typeId) 
+    values (6, '5', 'SPECIAL CODES', 4);
+insert into ATTR24_DIM (attr24id, NAME, TYPE, product_typeId) 
+    values (7, '6', 'SPECIAL CODES', 4);
+insert into ATTR24_DIM (attr24id, NAME, TYPE, product_typeId) 
+    values (8, '7', 'SPECIAL CODES', 4)
+/
+insert into ATTR24_DIM (attr24id, NAME, TYPE, product_typeId) 
+    values (9, '8', 'SPECIAL CODES', 4)
+/
+insert into ATTR24_DIM (attr24id, NAME, TYPE, product_typeId) 
+    values (10, '9', 'SPECIAL CODES', 4)
+/
