66ed884f628f192bf4921b0ab06769ea4e406f7b
nirmalab <>
2011-06-24 15:53:08 +0000

    Queries to update scoreLookUp table

--- oasx/LASLINK/DML/Master_data.sql
+++ oasx/LASLINK/DML/Master_data.sql
@@ -1,2 +1,11 @@
 INSERT INTO product_resource VALUES (7000,'TDCLOGO','/resources/logoLL.jpg')
 /
+
+UPDATE score_lookup SET test_level = '2-3'  WHERE  FRAMEWORK_CODE = 'LLEAB' AND test_level = '2'
+/
+UPDATE score_lookup SET test_level = '4-5'  WHERE  FRAMEWORK_CODE = 'LLEAB' AND test_level = '3'
+/
+UPDATE score_lookup SET test_level = '6-8'  WHERE  FRAMEWORK_CODE = 'LLEAB' AND test_level = '4'
+/
+UPDATE score_lookup SET test_level = '9-12'  WHERE  FRAMEWORK_CODE = 'LLEAB' AND test_level = '5'
+/
