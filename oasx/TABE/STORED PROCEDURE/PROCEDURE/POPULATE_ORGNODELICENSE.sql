dcddc608614e2dc972ed1ed165822e2b73bb6dd5
nirmalab <>
2011-07-22 15:16:12 +0000

    TABE procedure change

---
+++ oasx/TABE/STORED PROCEDURE/PROCEDURE/POPULATE_ORGNODELICENSE.sql
@@ -0,0 +1,276 @@
+CREATE OR REPLACE PROCEDURE POPULATE_ORGNODELICENSE IS
+
+  V_REC_COUNT          NUMBER(10) := 0;
+  TOPORGNODECATEGORYID INTEGER;
+  AVAILABLELIC         INTEGER := 0;
+  SESSIONMODELCOUNT    NUMBER(10) := 0;
+  SUBTESTMODELCOUNT    NUMBER(10) := 0;
+  RESERVEDLICENSECOUNT INTEGER := 0;
+  CONSUMEDLICENSECOUNT INTEGER := 0;
+  VCUSTOMERID          INTEGER := 0;
+
+  -- Get list of customer from customer_product_license table
+  CURSOR GETALLCUSTOMERID IS
+    SELECT DISTINCT (CUSTOMER_ID) AS CUSTOMERID
+      FROM CUSTOMER_PRODUCT_LICENSE;
+
+  --To get list of all product assigned to a customer
+  CURSOR GETCUSTOMERPRODUCTLICENSE(VCUSTOMERID INTEGER) IS
+    SELECT * FROM CUSTOMER_PRODUCT_LICENSE WHERE CUSTOMER_ID = VCUSTOMERID;
+
+  -- To get all Orgnode of customer along with childnode count to identify leaf node
+  CURSOR GETCUSTOMERORGNODE(VCUSTOMERID INTEGER, TOPORGNODECATEGORYID ORG_NODE_CATEGORY.ORG_NODE_CATEGORY_ID%TYPE) IS
+    SELECT DISTINCT NODE.ORG_NODE_ID AS ORGNODEID,
+                    NODE.CUSTOMER_ID AS CUSTOMERID,
+                    NODE.ORG_NODE_CATEGORY_ID AS ORGNODECATEGORYID,
+                    NODE.ORG_NODE_NAME AS ORGNODENAME,
+                    CAT.CATEGORY_NAME AS ORGNODECATEGORYNAME,
+                    COUNT(DISTINCT DESCENDANT_NODE.ORG_NODE_ID) - 1 AS CHILDNODECOUNT
+      FROM ORG_NODE          NODE,
+           ORG_NODE_CATEGORY CAT,
+           ORG_NODE_CATEGORY CUSTOMERCAT,
+           ORG_NODE_ANCESTOR DESCENDANTS,
+           ORG_NODE          DESCENDANT_NODE,
+           ORG_NODE_ANCESTOR DESCENDANTSCHILD
+     WHERE DESCENDANTS.ORG_NODE_ID = NODE.ORG_NODE_ID
+       AND CAT.ORG_NODE_CATEGORY_ID = NODE.ORG_NODE_CATEGORY_ID
+       AND CAT.ACTIVATION_STATUS = 'AC'
+       AND DESCENDANTS.ANCESTOR_ORG_NODE_ID =
+           (SELECT ORN.ORG_NODE_ID AS ORGNODEID
+              FROM ORG_NODE ORN
+             WHERE ORN.ORG_NODE_CATEGORY_ID = TOPORGNODECATEGORYID)
+       AND NODE.ORG_NODE_ID = DESCENDANTSCHILD.ANCESTOR_ORG_NODE_ID
+       AND DESCENDANTSCHILD.ORG_NODE_ID = DESCENDANT_NODE.ORG_NODE_ID
+       AND DESCENDANTSCHILD.NUMBER_OF_LEVELS >= 0
+       AND DESCENDANT_NODE.ACTIVATION_STATUS = 'AC'
+       AND NODE.ACTIVATION_STATUS = 'AC'
+     GROUP BY NODE.ORG_NODE_ID,
+              NODE.CUSTOMER_ID,
+              NODE.ORG_NODE_CATEGORY_ID,
+              NODE.ORG_NODE_NAME,
+              CAT.CATEGORY_NAME;
+
+BEGIN
+
+  FOR CUSTOMERIDLIST IN GETALLCUSTOMERID() LOOP
+    IF (CUSTOMERIDLIST.CUSTOMERID IS NOT NULL) THEN
+      VCUSTOMERID := CUSTOMERIDLIST.CUSTOMERID;
+      -- validate existance of a customer
+      BEGIN
+        SELECT 1
+          INTO V_REC_COUNT
+          FROM CUSTOMER
+         WHERE CUSTOMER.CUSTOMER_ID = VCUSTOMERID;
+      EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+          RAISE_APPLICATION_ERROR(-20000,
+                                  'Customer id [' || VCUSTOMERID ||
+                                  '] is not valid. Give Existing Id.');
+      END;
+    
+      BEGIN
+        -- To get categoryid for top orgnode of a customer
+        SELECT ONC.ORG_NODE_CATEGORY_ID
+          INTO TOPORGNODECATEGORYID
+          FROM ORG_NODE_CATEGORY ONC
+         WHERE ONC.CUSTOMER_ID = VCUSTOMERID
+           AND ONC.CATEGORY_LEVEL =
+               (SELECT MIN(ONC1.CATEGORY_LEVEL)
+                  FROM ORG_NODE_CATEGORY ONC1
+                 WHERE ONC.CUSTOMER_ID = ONC1.CUSTOMER_ID
+                   AND ONC1.ACTIVATION_STATUS = 'AC')
+           AND ONC.ACTIVATION_STATUS = 'AC'
+           AND ROWNUM < 2;
+      EXCEPTION
+        WHEN OTHERS THEN
+          DBMS_OUTPUT.PUT_LINE(VCUSTOMERID);
+      END;
+    
+      FOR PRODUCTLICENSE IN GETCUSTOMERPRODUCTLICENSE(VCUSTOMERID) LOOP
+        IF (PRODUCTLICENSE.PRODUCT_ID IS NOT NULL) THEN
+        
+          FOR NODE IN GETCUSTOMERORGNODE(VCUSTOMERID, TOPORGNODECATEGORYID) LOOP
+            IF (NODE.ORGNODEID IS NOT NULL) THEN
+            
+              -- check for the excistance of record for a customer orgnode and product. 
+              V_REC_COUNT := 0;
+              BEGIN
+                SELECT 1
+                  INTO V_REC_COUNT
+                  FROM CUSTOMER_ORGNODE_LICENSE COL
+                 WHERE COL.CUSTOMER_ID = VCUSTOMERID
+                   AND COL.ORG_NODE_ID = NODE.ORGNODEID
+                   AND COL.PRODUCT_ID = PRODUCTLICENSE.PRODUCT_ID;
+              EXCEPTION
+                WHEN NO_DATA_FOUND THEN
+                  V_REC_COUNT := 0;
+              END;
+            
+              --If record already exist then delete.
+              IF (V_REC_COUNT = 1) THEN
+                DELETE FROM CUSTOMER_ORGNODE_LICENSE COL
+                 WHERE COL.CUSTOMER_ID = VCUSTOMERID
+                   AND COL.ORG_NODE_ID = NODE.ORGNODEID
+                   AND COL.PRODUCT_ID = PRODUCTLICENSE.PRODUCT_ID;
+              END IF;
+            
+              IF (NODE.ORGNODECATEGORYID = TOPORGNODECATEGORYID AND
+                 NODE.CHILDNODECOUNT > 0) THEN
+                -- Insert record for Tor org node of a customer.
+                INSERT INTO CUSTOMER_ORGNODE_LICENSE
+                  (ORG_NODE_ID,
+                   CUSTOMER_ID,
+                   PRODUCT_ID,
+                   AVAILABLE,
+                   SUBTEST_MODEL,
+                   LICENSE_AFTER_LAST_PURCHASE,
+                   EMAIL_NOTIFY_FLAG)
+                VALUES
+                  (NODE.ORGNODEID,
+                   VCUSTOMERID,
+                   PRODUCTLICENSE.PRODUCT_ID,
+                   PRODUCTLICENSE.AVAILABLE,
+                   PRODUCTLICENSE.SUBTEST_MODEL,
+                   PRODUCTLICENSE.LICENSE_AFTER_LAST_PURCHASE,
+                   PRODUCTLICENSE.EMAIL_NOTIFY_FLAG);
+              END IF;
+            
+              IF (NODE.ORGNODECATEGORYID <> TOPORGNODECATEGORYID AND
+                 NODE.CHILDNODECOUNT > 0) THEN
+                -- Insert record for non leaf nodes.
+                INSERT INTO CUSTOMER_ORGNODE_LICENSE
+                  (ORG_NODE_ID,
+                   CUSTOMER_ID,
+                   PRODUCT_ID,
+                   SUBTEST_MODEL,
+                   LICENSE_AFTER_LAST_PURCHASE,
+                   EMAIL_NOTIFY_FLAG)
+                VALUES
+                  (NODE.ORGNODEID,
+                   VCUSTOMERID,
+                   PRODUCTLICENSE.PRODUCT_ID,
+                   PRODUCTLICENSE.SUBTEST_MODEL,
+                   PRODUCTLICENSE.LICENSE_AFTER_LAST_PURCHASE,
+                   PRODUCTLICENSE.EMAIL_NOTIFY_FLAG);
+              END IF;
+            
+              IF (NODE.CHILDNODECOUNT = 0) THEN
+              
+                IF (NODE.ORGNODECATEGORYID = TOPORGNODECATEGORYID) THEN
+                  AVAILABLELIC := PRODUCTLICENSE.AVAILABLE;
+                END IF;
+              
+                IF (PRODUCTLICENSE.SUBTEST_MODEL = 'T') THEN
+                  --Calulate reserved license count  for subtest model based on customerId, orgnodeId and customer ProductId.
+                  SELECT COUNT(SISS.TEST_ROSTER_ID)
+                    INTO SUBTESTMODELCOUNT
+                    FROM TEST_ADMIN              TADMIN,
+                         TEST_ROSTER             TR,
+                         PRODUCT                 PROD,
+                         STUDENT_ITEM_SET_STATUS SISS
+                   WHERE TADMIN.CUSTOMER_ID = VCUSTOMERID
+                     AND TADMIN.ACTIVATION_STATUS = 'AC'
+                     AND TADMIN.TEST_ADMIN_STATUS IN ('CU', 'FU')
+                     AND TR.TEST_ADMIN_ID = TADMIN.TEST_ADMIN_ID
+                     AND TR.ACTIVATION_STATUS = 'AC'
+                     AND TR.TEST_COMPLETION_STATUS = 'SC'
+                     AND TR.ORG_NODE_ID = NODE.ORGNODEID
+                     AND SISS.TEST_ROSTER_ID = TR.TEST_ROSTER_ID
+                     AND PROD.PRODUCT_ID = TADMIN.PRODUCT_ID
+                     AND (PRODUCTLICENSE.PRODUCT_ID = PROD.PRODUCT_ID OR
+                         PRODUCTLICENSE.PRODUCT_ID =
+                         PROD.PARENT_PRODUCT_ID);
+                
+                  RESERVEDLICENSECOUNT := SUBTESTMODELCOUNT;
+                  SUBTESTMODELCOUNT    := 0;
+                  
+                  --Calulate consumed license count  for subtest model based on customerId, orgnodeId and customer ProductId.
+                  SELECT COUNT(SISS.TEST_ROSTER_ID)
+                    INTO SUBTESTMODELCOUNT
+                    FROM TEST_ADMIN              TADMIN,
+                         TEST_ROSTER             TR,
+                         PRODUCT                 PROD,
+                         STUDENT_ITEM_SET_STATUS SISS
+                   WHERE TADMIN.CUSTOMER_ID = VCUSTOMERID
+                     AND TADMIN.ACTIVATION_STATUS = 'AC'
+                     AND TR.TEST_ADMIN_ID = TADMIN.TEST_ADMIN_ID
+                     AND TR.ACTIVATION_STATUS = 'AC'
+                     AND TR.TEST_COMPLETION_STATUS NOT IN ('SC', 'NT')
+                     AND TR.ORG_NODE_ID = NODE.ORGNODEID
+                     AND SISS.TEST_ROSTER_ID = TR.TEST_ROSTER_ID
+                     AND SISS.COMPLETION_STATUS NOT IN ('SC', 'NT')
+                     AND PROD.PRODUCT_ID = TADMIN.PRODUCT_ID
+                     AND (PRODUCTLICENSE.PRODUCT_ID = PROD.PRODUCT_ID OR
+                         PRODUCTLICENSE.PRODUCT_ID =
+                         PROD.PARENT_PRODUCT_ID);
+                
+                  CONSUMEDLICENSECOUNT := SUBTESTMODELCOUNT;
+                ELSE
+                  --Calulate reserved license count  for session model based on customerId, orgnodeId and customer ProductId.
+                  SELECT COUNT(DISTINCT TEST_ROSTER_ID)
+                    INTO SESSIONMODELCOUNT
+                    FROM TEST_ADMIN TADMIN, TEST_ROSTER TR, PRODUCT PROD
+                   WHERE TADMIN.CUSTOMER_ID = VCUSTOMERID
+                     AND TADMIN.ACTIVATION_STATUS = 'AC'
+                     AND TADMIN.TEST_ADMIN_STATUS IN ('CU', 'FU')
+                     AND TR.TEST_ADMIN_ID = TADMIN.TEST_ADMIN_ID
+                     AND TR.ACTIVATION_STATUS = 'AC'
+                     AND TR.TEST_COMPLETION_STATUS = 'SC'
+                     AND TR.ORG_NODE_ID = NODE.ORGNODEID
+                     AND PROD.PRODUCT_ID = TADMIN.PRODUCT_ID
+                     AND (PRODUCTLICENSE.PRODUCT_ID = PROD.PRODUCT_ID OR
+                         PRODUCTLICENSE.PRODUCT_ID =
+                         PROD.PARENT_PRODUCT_ID);
+                
+                  RESERVEDLICENSECOUNT := SESSIONMODELCOUNT;
+                  SESSIONMODELCOUNT    := 0;
+                  
+                  --Calulate consumed license count  for session model based on customerId, orgnodeId and customer ProductId
+                  SELECT COUNT(DISTINCT TEST_ROSTER_ID)
+                    INTO SESSIONMODELCOUNT
+                    FROM TEST_ADMIN TADMIN, TEST_ROSTER TR, PRODUCT PROD
+                   WHERE TADMIN.CUSTOMER_ID = VCUSTOMERID
+                     AND TADMIN.ACTIVATION_STATUS = 'AC'
+                     AND TR.TEST_ADMIN_ID = TADMIN.TEST_ADMIN_ID
+                     AND TR.ACTIVATION_STATUS = 'AC'
+                     AND TR.TEST_COMPLETION_STATUS NOT IN ('SC', 'NT')
+                     AND TR.ORG_NODE_ID = NODE.ORGNODEID
+                     AND PROD.PRODUCT_ID = TADMIN.PRODUCT_ID
+                     AND (PRODUCTLICENSE.PRODUCT_ID = PROD.PRODUCT_ID OR
+                         PRODUCTLICENSE.PRODUCT_ID =
+                         PROD.PARENT_PRODUCT_ID);
+                
+                  CONSUMEDLICENSECOUNT := SESSIONMODELCOUNT;
+                
+                END IF;
+              
+                -- Insert record for leaf node
+                INSERT INTO CUSTOMER_ORGNODE_LICENSE
+                  (ORG_NODE_ID,
+                   CUSTOMER_ID,
+                   PRODUCT_ID,
+                   AVAILABLE,
+                   RESERVED,
+                   CONSUMED,
+                   SUBTEST_MODEL,
+                   LICENSE_AFTER_LAST_PURCHASE,
+                   EMAIL_NOTIFY_FLAG)
+                VALUES
+                  (NODE.ORGNODEID,
+                   VCUSTOMERID,
+                   PRODUCTLICENSE.PRODUCT_ID,
+                   AVAILABLELIC,
+                   RESERVEDLICENSECOUNT,
+                   CONSUMEDLICENSECOUNT,
+                   PRODUCTLICENSE.SUBTEST_MODEL,
+                   PRODUCTLICENSE.LICENSE_AFTER_LAST_PURCHASE,
+                   PRODUCTLICENSE.EMAIL_NOTIFY_FLAG);
+              END IF;
+            END IF;
+          END LOOP;
+        END IF;
+      END LOOP;
+    END IF;
+  END LOOP;
+
+END POPULATE_ORGNODELICENSE;
