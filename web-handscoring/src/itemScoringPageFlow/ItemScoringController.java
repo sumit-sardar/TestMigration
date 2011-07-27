3bbc1875401023b53fd44da249f7599b6ac5405b
nirmalab <>
2011-07-27 12:14:16 +0000

    Changes for sidebar

--- web-handscoring/src/itemScoringPageFlow/ItemScoringController.java
+++ web-handscoring/src/itemScoringPageFlow/ItemScoringController.java
@@ -392,12 +392,12 @@ public class ItemScoringController extends PageFlowController {
 					"Configurable_Hand_Scoring")
 					&& cc.getDefaultValue().equals("T")) {
 				hasScoringConfigurable = true;
-				break;
+				//break;          
 			}
 			 if (cc.getCustomerConfigurationName().equalsIgnoreCase("Laslink_Customer")
 						&& cc.getDefaultValue().equals("T")) {
 					isLaslinkCustomer = true;
-					break;
+					//break;
 	            }
 		}
 		 this.setIslaslinkCustomer(isLaslinkCustomer);
