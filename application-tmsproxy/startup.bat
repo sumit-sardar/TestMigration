33a1af3863f3e276f7098655e5f91660de74a7bb
ncohen <>
2011-09-14 17:43:25 +0000

    port latest TMS changes

--- application-tmsproxy/startup.bat
+++ application-tmsproxy/startup.bat
@@ -1,6 +1,7 @@
 @echo off
-c:
-cd c:\oas\application-tmsproxy
+cd /d %~dp0
+set "CURRENT_DIR=%cd%"
+cd %CURRENT_DIR%
 rem start call "storage\apache-cassandra-0.7.6-2\bin\cassandra.bat"
 rem start java -cp ./storage/hsqldb/lib/hsqldb.jar org.hsqldb.server.Server --database.0 file:OAS --dbname.0 oas
 cd .\server\apache-tomcat-6.0.29\bin\
