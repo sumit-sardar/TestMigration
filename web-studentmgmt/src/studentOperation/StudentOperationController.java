828f3e539c27b189ecb3e8c5aaec0df7e37b2ab6
ayanb <>
2011-09-30 21:53:08 +0000

    fixed annotation processing error

--- web-studentmgmt/src/studentOperation/StudentOperationController.java
+++ web-studentmgmt/src/studentOperation/StudentOperationController.java
@@ -1519,7 +1519,7 @@ public class StudentOperationController extends PageFlowController {
 	        @Jpf.Forward(name = "installSoftwareLink", path = "services_installSoftware.do"),
 	        @Jpf.Forward(name = "downloadTestLink", path = "services_downloadTest.do"),
 	        @Jpf.Forward(name = "uploadDataLink", path = "services_uploadData.do"),
-	        @Jpf.Forward(name = "downloadDataLink", path = "services_downloadData.do"),
+	        @Jpf.Forward(name = "downloadDataLink", path = "services_downloadData.do")
 	    }) 
 	protected Forward services()
 	{
