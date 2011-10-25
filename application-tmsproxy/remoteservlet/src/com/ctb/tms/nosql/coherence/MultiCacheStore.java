4b4391b5cd4e3ec5ea64169a08f4cb1f3f3a648a
ncohen <>
2011-10-25 21:57:08 +0000

    more push replication work

--- application-tmsproxy/remoteservlet/src/com/ctb/tms/nosql/coherence/MultiCacheStore.java
+++ application-tmsproxy/remoteservlet/src/com/ctb/tms/nosql/coherence/MultiCacheStore.java
@@ -3,13 +3,22 @@ package com.ctb.tms.nosql.coherence;
 import java.util.Collection;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
+
+import com.ctb.tms.nosql.coherence.push.TMSRemoteAddressProvider;
+import com.oracle.coherence.patterns.pushreplication.PublishingCacheStore;
 import com.tangosol.net.cache.CacheStore;
 
-public class OracleCacheStore implements CacheStore {
+public class MultiCacheStore implements CacheStore {
+	
+	static Logger logger = Logger.getLogger(MultiCacheStore.class);
 	
 	private CacheStore store;
+	private PublishingCacheStore pushStore;
 	
-	public OracleCacheStore(String cacheName) {
+	public MultiCacheStore(String cacheName) {
+		pushStore = new PublishingCacheStore(cacheName);
+		
 		if("OASRosterCache".equals(cacheName)) store = new RosterCacheStore(cacheName); 
 		else if("OASManifestCache".equals(cacheName)) store = new ManifestCacheStore(cacheName); 
 		else if("OASResponseCache".equals(cacheName)) store = new ResponseCacheStore(cacheName);
@@ -17,7 +26,7 @@ public class OracleCacheStore implements CacheStore {
 		else if("ADSItemSetCache".equals(cacheName)) store = new ItemSetCacheStore(cacheName);
     }
 	
-	public OracleCacheStore() {
+	public MultiCacheStore() {
     }
 
     // ----- CacheStore Interface -----
@@ -29,13 +38,28 @@ public class OracleCacheStore implements CacheStore {
     public void store(Object oKey, Object oValue) {
     	store.store(oKey, oValue);
     }
+    
+    public void store(com.tangosol.util.BinaryEntry entry) {
+    	logger.info("Write to push replication store");
+    	pushStore.store(entry);
+    }
 
     public void erase(Object oKey) {
     	store.erase(oKey);
     }
+    
+    public void erase(com.tangosol.util.BinaryEntry entry) {
+    	logger.info("Delete to push replication store");
+    	pushStore.erase(entry);
+    }
 
 	public void eraseAll(Collection colKeys) {
 		store.eraseAll(colKeys);
+	}
+	
+	public void eraseAll(java.util.Set setBinEntries) {
+		logger.info("Batch delete to push replication store");
+		pushStore.eraseAll(setBinEntries);
 	}
 
 	public Map loadAll(Collection colKeys) {
@@ -44,5 +68,10 @@ public class OracleCacheStore implements CacheStore {
 
 	public void storeAll(Map mapEntries) {
 		store.storeAll(mapEntries);
+	}
+	
+	public void storeAll(java.util.Set setBinEntries) {
+		logger.info("Batch write to push replication store");
+		pushStore.storeAll(setBinEntries);
 	}
 }
--- application-tmsproxy/remoteservlet/src/com/ctb/tms/nosql/coherence/push/TMSRemoteAddressProvider.java
+++ application-tmsproxy/remoteservlet/src/com/ctb/tms/nosql/coherence/push/TMSRemoteAddressProvider.java
@@ -1,6 +1,10 @@
 package com.ctb.tms.nosql.coherence.push;
 
 import java.net.InetSocketAddress;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.ResourceBundle;
 
 import org.apache.log4j.Logger;
 
@@ -9,18 +13,117 @@ import com.tangosol.net.AddressProvider;
 public class TMSRemoteAddressProvider implements AddressProvider {
 
 	static Logger logger = Logger.getLogger(TMSRemoteAddressProvider.class);
+	static HashMap<String, RemoteNode> remoteMap = new HashMap<String, RemoteNode>(10);
+	static int nextIndex = 1;
+	RemoteNode remote = null;
+	
+	private static class RemoteNode {
+		private String host;
+		private int port;
+		private boolean status;
+		private long lastStatusCheck;
+		private static final long STATUS_CHECK_INTERVAL = 60000;
+		
+		public String getHost() {
+			return host;
+		}
+		public void setHost(String host) {
+			this.host = host;
+		}
+		public int getPort() {
+			return port;
+		}
+		public void setPort(int port) {
+			this.port = port;
+		}
+		public boolean getStatus() {
+			if(!status && (System.currentTimeMillis() - lastStatusCheck) > STATUS_CHECK_INTERVAL) {
+				status = true;
+			}
+			return status;
+		}
+		public void setStatus(boolean status) {
+			this.status = status;
+		}
+		public long getLastStatusCheck() {
+			return lastStatusCheck;
+		}
+		public void setLastStatusCheck(long lastStatusCheck) {
+			this.lastStatusCheck = lastStatusCheck;
+		}
+	}
+	
+	static {
+		ResourceBundle rb = ResourceBundle.getBundle("pushreplication");
+		Enumeration<String> keys = rb.getKeys();
+		while(keys.hasMoreElements()) {
+			String fullKey = keys.nextElement();
+			String key = fullKey;
+			System.out.println("Full key: " + key);
+			if(key.startsWith("coherence.remote.")) {
+				key = key.substring(17, key.length());
+				System.out.println("Remote key: " + key);
+				String node = key.substring(0, key.indexOf("."));
+				System.out.println("Node: " + node);
+				key = key.substring(key.indexOf(".") + 1, key.length());
+				System.out.println("Final key: " + key);
+				if(key.startsWith("hostname")) {
+					String hostname = rb.getString(fullKey);
+					RemoteNode remote = remoteMap.get(node);
+					if(remote == null) {
+						remote = new RemoteNode();
+					}
+					remote.setHost(hostname);
+					remote.setStatus(true);
+					remoteMap.put(node, remote);
+				} else if(key.startsWith("port")) {
+					String port = rb.getString(fullKey);
+					RemoteNode remote = remoteMap.get(node);
+					if(remote == null) {
+						remote = new RemoteNode();
+					}
+					remote.setPort(Integer.parseInt(port));
+					remote.setStatus(true);
+					remoteMap.put(node, remote);
+				}
+			}
+		}
+	}
 	
 	public void accept() {
-		logger.warn("Address accepted.");
+		remote.setStatus(true);
+		remote.setLastStatusCheck(System.currentTimeMillis());
+		logger.info("Address accepted: " + remote.getHost() + ":" + remote.getPort());
 	}
 
 	public InetSocketAddress getNextAddress() {
-		InetSocketAddress addr = new InetSocketAddress("nj09mhe5335", 8089);
-		return addr;
+		logger.info("request for address");
+		int tries = remoteMap.size();
+		for(int i=0;i<tries;i++) {
+			synchronized(TMSRemoteAddressProvider.class) {
+				if(nextIndex > tries) nextIndex = 1;
+				remote = remoteMap.get(String.valueOf(nextIndex));
+				nextIndex += 1;
+			}
+			if(remote != null && remote.getStatus()) {
+				break;
+			} else {
+				remote = null;
+			}
+		}
+		if(remote != null) {
+			InetSocketAddress addr = new InetSocketAddress(remote.getHost(), remote.getPort());
+			logger.info("Returned address: " + remote.getHost() + ":" + remote.getPort());
+			return addr;
+		}
+		logger.warn("Could not obtain remote address, returning null!");
+		return null;
 	}
 
 	public void reject(Throwable arg0) {
-		logger.warn("Address rejected!");
+		remote.setStatus(false);
+		remote.setLastStatusCheck(System.currentTimeMillis());
+		logger.warn("Address rejected: " + remote.getHost() + ":" + remote.getPort());
 	}
 
 }
