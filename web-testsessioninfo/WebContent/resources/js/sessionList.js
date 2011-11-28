1d232e1b1027024a88694c71373ebe6408110a93
nirmalab <>
2011-11-28 13:06:36 +0000

    For new schedule testSession ui changes : disable dropdown select box  when single item is present

--- web-testsessioninfo/WebContent/resources/js/sessionList.js
+++ web-testsessioninfo/WebContent/resources/js/sessionList.js
@@ -783,6 +783,11 @@ function createSingleNodeSelectedTree(jsondata) {
 			}
 		}
 		$(ddl).html(optionHtml);
+		if(optionList.length==1) {
+			ddl.disabled=true;
+		} else {
+			ddl.disabled=false;
+		}
 	}
 	
 	
