4718221b931566d6290ec2c061230eb2f6b632aa
nirmalab <>
2011-12-21 07:57:08 +0000

    Changes for defect #67657 - "Students added" message

--- web-testsessioninfo/WebContent/resources/js/sessionList.js
+++ web-testsessioninfo/WebContent/resources/js/sessionList.js
@@ -651,6 +651,7 @@ function createSingleNodeSelectedTree(jsondata) {
 			resetStudentSelection();
 			resetOnSelectTestSessionData();
 			resetProctor();
+			$('#studentAddDeleteInfo').hide();
 
 		}
 		$("#"+dailogId).dialog("close");
