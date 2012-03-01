a11385e754cfc18f7d31f4bc340af3b1a5fa1ad7
Sumit Sardar <sumit.sardar@mheducation.com>
2012-03-01 16:44:56 +0000

    updated for access code

--- web-testsessioninfo/src/com/ctb/testSessionInfo/utils/ScheduleTestVo.java
+++ web-testsessioninfo/src/com/ctb/testSessionInfo/utils/ScheduleTestVo.java
@@ -349,7 +349,7 @@ public class ScheduleTestVo implements Serializable{
 		}
 		
 		if(maxSubtestCount>0){
-			this.accessCodeList = scheduleTest.getFixedNoAccessCode(maxSubtestCount);
+			this.accessCodeList = scheduleTest.getFixedNoAccessCode(maxSubtestCount+2);
 		}
 		
 	}
