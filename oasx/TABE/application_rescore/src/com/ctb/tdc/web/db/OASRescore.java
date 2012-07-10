a1791eef62e62a3913c554a9572fe866ef9f70b8
nirmalab <>
2012-07-10 14:27:39 +0000

    *** empty log message ***

--- oasx/TABE/application_rescore/src/com/ctb/tdc/web/db/OASRescore.java
+++ oasx/TABE/application_rescore/src/com/ctb/tdc/web/db/OASRescore.java
@@ -25,7 +25,8 @@ public class OASRescore {
 		+"AND tr.test_roster_id = ? "
 		+"AND stu.student_id = tr.student_id";
 	private static final String RESTART_RESPONSES_SQL = "select  ir.item_id as itemId, its.subject contentArea, ir.response_seq_num as responseSeqNum,  ir.student_marked as studentMarked,  i.item_type as itemType,  isi.item_sort_order as itemSortOrder,  ir.response as response, TO_CLOB(decode (i.answer_area,'AudioItem', decode(length(icr.constructed_response),'','',ir.test_roster_id || '_' || ir.item_id), DBMS_LOB.SUBSTR(icr.constructed_response, 4000, 1))) as constructedResponse, ir.response_elapsed_time as responseElapsedTime,  decode(ir.response, i.correct_answer, 1, 0) as score,  i.ads_item_id as eid, decode(i.answer_area,'AudioItem','T','F') as audioItem from  item_response ir,  item i,  item_response_cr icr,  item_set_item isi  ,item_set its where  ir.test_roster_id = ?  and ir.item_set_id = ?  and ir.item_id = i.item_id and its.item_set_id = ir.item_set_id and ir.item_set_id = isi.item_set_id  and ir.item_id = isi.item_id AND its.item_set_type = 'TD' and its.sample = 'F' and ir.response_seq_num =  (select  max(ir1.response_seq_num)  from  item_response ir1  where  ir1.item_set_id = ir.item_set_id  and ir1.item_id = ir.item_id  and ir1.test_roster_id = ir.test_roster_id) \t  AND ir.test_roster_id = icr.test_roster_id (+) \t  AND ir.item_set_id = icr.item_set_id (+) \t  AND ir.item_id = icr.item_id (+) order by  ir.response_seq_num asc";
-
+	private static final String UPDATE_SCORE="UPDATE student_item_set_status siss SET siss.objective_score = ? ,siss.ability_score = ? ,siss.sem_score = ? WHERE siss.item_set_id=? AND siss.test_roster_id=?";
+	private static final String UPDATE_RESCORESTATUS="UPDATE test_roster_temp tr SET tr.rescored = 'T' WHERE tr.test_roster_id=?";
 	
 	public void getRoster(int rosterId, Connection conn){
 		PreparedStatement stmt;
@@ -78,7 +79,7 @@ public class OASRescore {
     }
 	
 	private void generateRosterData (Connection conn, AuthenticationData [] authDataArray) throws Exception {
-        ArrayList itemSetData = new ArrayList();
+		ArrayList itemSetData = new ArrayList();
     	AuthenticationData authData = null;
         int testRosterId = -1;
         for(int a=0;authDataArray != null && a<authDataArray.length;a++) {
@@ -96,14 +97,64 @@ public class OASRescore {
 		        if(RosterData.restartItemCount>0){
 			        CATEngineProxy.initCAT(cArea);
 		        	CATEngineProxy.restartCAT(RosterData.restartItemCount,RosterData.restartItemsArr,RosterData.restartItemsRawScore);
-		        	CATEngineProxy.getObjScore();
-		        	CATEngineProxy.getSEM();
-		        	CATEngineProxy.getAbilityScore();
+		        	
+		        	Double abilityScore = CATEngineProxy.getAbilityScore();
+        			Double sem = CATEngineProxy.getSEM();
+        			String objScore = CATEngineProxy.getObjScore();	
+        			
+        			updateScore(abilityScore,sem,objScore,conn,testRosterId,iSet.getId());
+        			updateRescoredStatus(conn,testRosterId);
 		        }
 	        }
 	        System.out.println("restartItemsArr: item "+ RosterData.restartItemCount+RosterData.restartItemsArr+RosterData.restartItemsRawScore );
         } 
     }
+	
+	private void updateScore(Double abilityScore,Double sem,String objScore,Connection con, int testRosterId, int itemSetId){
+		PreparedStatement stmt1 = null;
+		try{
+			stmt1 = con.prepareStatement(UPDATE_SCORE);
+			stmt1.setString(1, objScore);
+			stmt1.setFloat(2, abilityScore.floatValue());
+			stmt1.setFloat(3, sem.floatValue());
+			stmt1.setInt(4, itemSetId);
+			stmt1.setInt(5, testRosterId);
+			
+			stmt1.executeUpdate();
+			con.commit();
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			//manifests = null;
+		} finally {
+			try {
+				if(stmt1 != null) stmt1.close();
+			} catch (Exception e) {
+				System.err.println("Exception occured in update score"+e);
+			}
+		}
+	}
+	
+	private void updateRescoredStatus(Connection con, int testRosterId){
+		PreparedStatement stmt1 = null;
+		try{
+			stmt1 = con.prepareStatement(UPDATE_RESCORESTATUS);
+			stmt1.setInt(1, testRosterId);
+			
+			stmt1.executeUpdate();
+			con.commit();
+			
+		}catch (Exception e) {
+			e.printStackTrace();
+			//manifests = null;
+		} finally {
+			try {
+				if(stmt1 != null) stmt1.close();
+			} catch (Exception e) {
+				System.err.println("Exception occured in update score"+e);
+			}
+		}
+	}
 	
 	private ArrayList getItemSetData(Connection con, String testRosterId){
 		PreparedStatement stmt1 = null;
