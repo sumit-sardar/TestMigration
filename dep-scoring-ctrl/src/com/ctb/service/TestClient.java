e8a1df3233709622dc9ff8f0b2e11bc2ad79786e
mishra <>
2012-07-30 21:52:57 +0000

    Changes for user story - 'TN on Acuity – OAS '

---
+++ dep-scoring-ctrl/src/com/ctb/service/TestClient.java
@@ -0,0 +1,47 @@
+package com.ctb.service;
+
+import java.util.ResourceBundle;
+
+import org.apache.axis2.context.ConfigurationContext;
+import org.apache.axis2.context.ConfigurationContextFactory;
+
+import com.mcgraw_hill.ctb.acuity.scoring.ScoringServiceStub;
+import com.mcgraw_hill.ctb.acuity.scoring.ScoringServiceStub.AuthenticatedUser;
+import com.mcgraw_hill.ctb.acuity.scoring.ScoringServiceStub.ProcessStudentScore;
+import com.mcgraw_hill.ctb.acuity.scoring.ScoringServiceStub.ProcessStudentScoreResponse;
+import com.mcgraw_hill.ctb.acuity.scoring.ScoringServiceStub.ScoringStatus;
+import com.mcgraw_hill.ctb.acuity.scoring.ScoringServiceStub.StudentScore;
+
+public class TestClient {
+	public static void main(String [] args) {
+		      try {
+		    	  ResourceBundle rb = ResourceBundle.getBundle("webServiceUrls");
+		    	  String endPointUrl = rb.getString("url");
+		    	  StudentScore stuScore = new StudentScore();
+		    	  stuScore.setStudentId(1644566);
+		    	  stuScore.setSessionId(234672);
+		    	  stuScore.setFormId("G");
+		    	  stuScore.setLevelId(13);
+		    	  ProcessStudentScore pss = new ProcessStudentScore();
+		    	  pss.setScore(stuScore);
+		    	  AuthenticatedUser  user = new AuthenticatedUser();
+		    	  user.setUsername("");
+		    	  user.setUsername("");
+		    	  
+		    	  pss.setUser(user);
+		    	  String url = "http://192.168.14.136:8080/host/services/ScoringService";
+		    	  //ConfigurationContext ctx = ConfigurationContextFactory.createConfigurationContext("?wsdl", url);
+		    	  ScoringServiceStub stub = new ScoringServiceStub(url);
+		    	  ProcessStudentScoreResponse resp = stub.processStudentScore(pss);
+		    	  ScoringStatus status = resp.get_return();
+		    	  System.out.println("status.getStudentId() -> " + status.getStudentId());
+		    	  System.out.println("status.getSessionId() -> " + status.getSessionId());
+		    	  System.out.println("status.getErrorMsg() -> " + status.getErrorMsg());
+		    	  System.out.println("status.getStatus() -> " + status.getStatus());
+		        } catch (Exception e) {
+		        System.err.println(e.toString());
+		        e.printStackTrace();
+		      }
+		    }
+
+}
