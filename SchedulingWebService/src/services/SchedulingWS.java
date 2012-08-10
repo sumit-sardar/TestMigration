425fbf3d92fa3dc3fb2f4c94c8f37130c9e42ef8
santhana <>
2012-08-10 15:34:33 +0000

    For update assessment

--- SchedulingWebService/src/services/SchedulingWS.java
+++ SchedulingWebService/src/services/SchedulingWS.java
@@ -259,11 +259,11 @@ public class SchedulingWS implements Serializable {
 	    		boolean createNewStudent = true;
 	    		boolean removedStudent = false;
 	    		
-	    		if (studentId != null && student.getRemoveStatus() != null && !student.getRemoveStatus()) {  // for update student
+	    		if (studentId != null && (student.getRemoveStatus() == null || !student.getRemoveStatus())) {  // for update student
 	    			Student studentDetail = getStudent(studentId);
 	    			studentId = studentDetail.getStudentId();
 	    			createNewStudent = false;
-	    		} else if (studentId != null && student.getRemoveStatus() !=null && student.getRemoveStatus()){  // for remove student
+	    		} else if (studentId != null && (student.getRemoveStatus() != null && student.getRemoveStatus())){  // for remove student
 	    			if(!validateStudentForRemove(session, student)){
 	    				createNewStudent = false;
 	    			} else	if (!deleteStudent(student,  session.getSessionId())){
