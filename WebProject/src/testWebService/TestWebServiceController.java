5bcdaa80fd08bf0a964da435b322dd4e0e6c96ca
ttruong <>
2012-08-27 18:24:29 +0000

    WS test

--- WebProject/src/testWebService/TestWebServiceController.java
+++ WebProject/src/testWebService/TestWebServiceController.java
@@ -1,13 +1,22 @@
 package testWebService;
 
 import java.util.Date;
+import java.util.ResourceBundle;
 
 import javax.servlet.http.HttpSession;
 
+import org.apache.axis2.context.ConfigurationContext;
+import org.apache.axis2.context.ConfigurationContextFactory;
 import org.apache.beehive.netui.pageflow.Forward;
 import org.apache.beehive.netui.pageflow.PageFlowController;
 import org.apache.beehive.netui.pageflow.annotations.Jpf;
 import org.apache.beehive.controls.api.bean.Control;
+
+import com.mcgraw_hill.ctb.acuity.scoring.ScoringServiceStub;
+import com.mcgraw_hill.ctb.acuity.scoring.ScoringServiceStub.AuthenticatedUser;
+import com.mcgraw_hill.ctb.acuity.scoring.ScoringServiceStub.ProcessStudentScore;
+import com.mcgraw_hill.ctb.acuity.scoring.ScoringServiceStub.ScoringStatus;
+import com.mcgraw_hill.ctb.acuity.scoring.ScoringServiceStub.StudentScore;
 
 import dto.Accommodation;
 import dto.SecureUser;
@@ -449,5 +458,56 @@ public class TestWebServiceController extends PageFlowController
     	
 		return infoText;
     }
-    
+
+    @Jpf.Action(forwards = { 
+            @Jpf.Forward(name = "success", path = "testProduction.jsp") 
+        }) 
+    protected Forward testProduction()
+    {
+    	
+      try {
+    	  ResourceBundle rb = ResourceBundle.getBundle("webServiceUrls");
+    	  String endPointUrl = rb.getString("url");
+    	  
+    	  StudentScore stuScore = new StudentScore();
+    	  stuScore.setStudentId(1644566);
+    	  stuScore.setSessionId(234672);
+    	  stuScore.setFormId("G");
+    	  stuScore.setLevelId(13);
+    	  ProcessStudentScore pss = new ProcessStudentScore();
+    	  pss.setScore(stuScore);
+    	  AuthenticatedUser  user = new AuthenticatedUser();
+    	  user.setUsername("");
+    	  user.setUsername("");
+    	  
+    	  pss.setUser(user);
+    	  String url = "http://192.168.14.136:8080/host/services/ScoringService";
+    	  //String url = "http://151.108.140.171/bredexsoap/services/ScoringService?wsdl";
+    	  
+    	  //ConfigurationContext ctx = ConfigurationContextFactory.createConfigurationContext("?wsdl", url);
+  		  //final ConfigurationContext ctx = ConfigurationContextFactory.createConfigurationContextFromFileSystem("./repo",null);
+  		  final ConfigurationContext ctx = ConfigurationContextFactory.createConfigurationContextFromFileSystem("C:\\workspace\\dep-scoring-ctrl\\repo", null);
+    	  
+    	  /*
+  		  final ConfigurationContext ctx = 
+  			  ConfigurationContextFactory.createConfigurationContextFromFileSystem("C:\\workspace\\dep-scoring-ctrl\\repo", 
+  					"C:\\workspace\\dep-scoring-ctrl\\repo\\axis2.xml");
+  					*/
+    	  ScoringServiceStub stub = new ScoringServiceStub(ctx, url);
+  		  stub._getServiceClient().engageModule("logging");
+    	  ScoringStatus status = stub.processStudentScore(user, stuScore);
+    	  System.out.println("status.getStudentId() -> " + status.getStudentId());
+    	  System.out.println("status.getSessionId() -> " + status.getSessionId());
+    	  System.out.println("status.getErrorMsg() -> " + status.getErrorMsg());
+    	  System.out.println("status.getStatus() -> " + status.getStatus());
+        } 
+      	catch (Exception e) {
+      		System.err.println(e.toString());
+      		e.printStackTrace();
+      	}
+    	
+        return new Forward("success");
+    }
+
+     
  }
