a96d7097b0e3719405b422c802a4afd28ec4e0c6
snandi <>
2012-09-21 12:58:23 +0000

    Rescore Utility : Spelling correct

--- web-scoring/src/job/RescoreJob.java
+++ web-scoring/src/job/RescoreJob.java
@@ -19,7 +19,7 @@ public class RescoreJob implements Job {
 	@Override
 	public void execute(JobExecutionContext arg0) throws JobExecutionException {
 
-		System.out.println("Schedular For Rescore Fired@" + new Date());
+		System.out.println("Scheduler For Rescore Fired@" + new Date());
 		try {
 			Configuration configuration = Configuration.getConfiguration();
 			ScheduleRosterBO bo = new ScheduleRosterBO();
