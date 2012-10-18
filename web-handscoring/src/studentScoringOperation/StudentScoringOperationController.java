c44405d315c3832adfef03e7749d85ffe316b826
snandi <>
2012-10-18 15:36:23 +0000

    Changes for Laslink Student Data Export User story

--- web-handscoring/src/studentScoringOperation/StudentScoringOperationController.java
+++ web-handscoring/src/studentScoringOperation/StudentScoringOperationController.java
@@ -1200,6 +1200,7 @@ private void setUpAllUserPermission(CustomerConfiguration [] customerConfigurati
 		this.getSession().setAttribute("canRegisterStudent", new Boolean(TABECustomer && validUser));
 		this.getSession().setAttribute("hasRapidRagistrationConfigured", new Boolean(TABECustomer&&(adminUser || adminCoordinatorUser) )); //For Student Registration
 		this.getSession().setAttribute("hasResetTestSession", new Boolean(hasResetTestSession && ((isOKCustomer && isTopLevelAdmin)||(laslinkCustomer && isTopLevelAdmin)||(isGACustomer && adminUser))));
+		this.getSession().setAttribute("showDataExportTab",laslinkCustomer);
     }
 	
 	private boolean isTopLevelUser(){
@@ -1585,7 +1586,9 @@ private void setUpAllUserPermission(CustomerConfiguration [] customerConfigurati
 	        @Jpf.Forward(name = "installSoftwareLink", path = "services_installSoftware.do"),
 	        @Jpf.Forward(name = "downloadTestLink", path = "services_downloadTest.do"),
 	        @Jpf.Forward(name = "uploadDataLink", path = "services_uploadData.do"),
-	        @Jpf.Forward(name = "downloadDataLink", path = "services_downloadData.do")
+	        @Jpf.Forward(name = "downloadDataLink", path = "services_downloadData.do"),
+	        @Jpf.Forward(name = "exportDataLink", path = "services_dataExport.do"),
+			@Jpf.Forward(name = "viewStatusLink", path="services_viewStatus.do")   
 	    }) 
 	protected Forward services()
 	{
@@ -1594,6 +1597,37 @@ private void setUpAllUserPermission(CustomerConfiguration [] customerConfigurati
 		
 	    return new Forward(forwardName);
 	}
+	
+	@Jpf.Action()
+    protected Forward services_dataExport()
+    {
+    	try
+    	{
+    		String url = "/ExportWeb/dataExportOperation/services_dataExport.do";
+    		getResponse().sendRedirect(url);
+    	}
+    	catch (IOException ioe)
+        {
+            System.err.print(ioe.getStackTrace());
+        }
+    	return null;
+    }
+	
+	@Jpf.Action()
+	protected Forward services_viewStatus()
+	{
+        try
+        {
+            String url = "/ExportWeb/dataExportOperation/beginViewStatus.do";
+            getResponse().sendRedirect(url);
+        } 
+        catch (IOException ioe)
+        {
+            System.err.print(ioe.getStackTrace());
+        }
+        return null;
+	}
+ 
 	
     @Jpf.Action()
     protected Forward services_resetTestSession()
--- web-organizationmgmt/src/downloadOperation/DownloadOperationController.java
+++ web-organizationmgmt/src/downloadOperation/DownloadOperationController.java
@@ -499,7 +499,9 @@ public class DownloadOperationController extends PageFlowController {
     		@Jpf.Forward(name = "installSoftwareLink", path = "services_installSoftware.do"),
     		@Jpf.Forward(name = "downloadTestLink", path = "services_downloadTest.do"),
     		@Jpf.Forward(name = "uploadDataLink", path = "services_uploadData.do"),
-    		@Jpf.Forward(name = "downloadDataLink", path = "services_downloadData.do")
+    		@Jpf.Forward(name = "downloadDataLink", path = "services_downloadData.do"),
+    		@Jpf.Forward(name = "exportDataLink", path = "services_dataExport.do"),
+    		@Jpf.Forward(name = "viewStatusLink", path = "services_viewStatus.do")
     }) 
     protected Forward services()
     {
@@ -508,6 +510,39 @@ public class DownloadOperationController extends PageFlowController {
 
     	return new Forward(forwardName);
     }
+    
+    @Jpf.Action()
+    protected Forward services_dataExport()
+    {
+    	try
+    	{
+    		String url = "/ExportWeb/dataExportOperation/services_dataExport.do";
+    		getResponse().sendRedirect(url);
+    	}
+    	catch (IOException ioe)
+        {
+            System.err.print(ioe.getStackTrace());
+        }
+    	return null;
+    }
+    
+
+    @Jpf.Action()
+	protected Forward services_viewStatus()
+	{
+        try
+        {
+            String url = "/ExportWeb/dataExportOperation/beginViewStatus.do";
+            getResponse().sendRedirect(url);
+        } 
+        catch (IOException ioe)
+        {
+            System.err.print(ioe.getStackTrace());
+        }
+        return null;
+	}
+    
+    
 
     @Jpf.Action()
     protected Forward services_resetTestSession()
