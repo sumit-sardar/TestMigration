f405ada8b98e176fc58d1d117e4c089b3ccdb449
Sumit Sardar <sumit.sardar@mheducation.com>
2013-06-21 10:35:03 +0000

    For DEx SSO

--- web-dexsso/src/ManageSSOController.java
+++ web-dexsso/src/ManageSSOController.java
@@ -29,33 +29,40 @@ public class ManageSSOController extends PageFlowController
 	)
     protected Forward begin()
     {
+    	System.out.println("ManageSSO 32");
     	Forward result = globalApp.logout();
     	try {
-    		
+    		System.out.println("ManageSSO 35");
     		Cookie cookie = new Cookie( "TAS_SESSIONID", "00000000000000000000" );
         	cookie.setMaxAge( -42000 );
         	this.getResponse().addCookie(cookie);
-        	this.getResponse().setHeader("Set-Cookie", "TAS_SESSIONID=00000000000000000000;path=" + this.getRequest().getContextPath() + "lm_defect" );
-     		
-     /*   	String tokenValue = this.getRequest().getParameter("token");
+        	String tokenValue = this.getRequest().getParameter("oamSessionID");
+        	System.out.println("Token - " + tokenValue);
         	tokenValue = URLDecoder.decode(tokenValue,"UTF-8");
+        	System.out.println("Token after URL Decode - " + tokenValue);
             //Base 64
         	BASE64Encoder base64 =  new BASE64Encoder();
             tokenValue = base64.encode(tokenValue.getBytes());
+            System.out.println("Token after Base 64 encode - " + tokenValue);
+            tokenValue=tokenValue.replaceAll("\\r\\n", "");
+            System.out.println("Token after replacing CRLF - " + tokenValue);
             Cookie encodeCookie = new Cookie( "DExPerimeterAtnToken", tokenValue);
-        	cookie.setMaxAge( -42000 );
+        	encodeCookie.setMaxAge( -42000 );
         	this.getResponse().addCookie(encodeCookie);
-      */     
+            this.getResponse().setHeader("Set-Cookie", "TAS_SESSIONID=00000000000000000000;path=" + this.getRequest().getContextPath() + "lm_defect" );
+            
+    	
     	} catch (Exception e) {
     		
     		e.printStackTrace();
+    		System.out.println("ManageSSO 53");
     	}
         
          
         
 
-
-    	return result;
+    	System.out.println("ManageSSO 59");
+    	return new Forward("success");
     }
     
 }
