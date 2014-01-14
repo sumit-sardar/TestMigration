b536b62248b5ca903f62dfeb76b74a9880724fda
psaha <>
2014-01-14 08:43:06 +0000

    Reverting back to the previous version

--- web-studentmgmt/WebContent/resources/js/hierarchy.js
+++ web-studentmgmt/WebContent/resources/js/hierarchy.js
@@ -3160,23 +3160,7 @@ function setSelectedValue(selectObj, valueToSet) {
 		
 		}	
 		if (objArray.attr.cid == leafNodeCategoryId){
-			var isWVStateUser = $("#addStudentEnable").val();
-			var isWVCustomer = $("#isWVCustomer").val();
-			if(isWVCustomer == 'true')
-			{
-				if( isWVStateUser == 'true')
-				{
-					liElement.innerHTML = "<ins class=\"jstree-icon\">&nbsp;</ins><a href=\"#\" class=\"\"><ins class=\"jstree-checkbox\" style=\"display: inline-block;\">&nbsp;</ins><ins class=\"jstree-icon\">&nbsp;</ins>" + objArray.data + "</a> ";
-				}
-				else
-				{
-					liElement.innerHTML = "<ins class=\"jstree-icon\">&nbsp;</ins><ins class=\"jstree-icon\">&nbsp;</ins>" + objArray.data + "</a> ";
-				}
-			}
-			else
-			{
-			liElement.innerHTML = "<ins class=\"jstree-icon\">&nbsp;</ins><a href=\"#\" class=\"\"><ins class=\"jstree-checkbox\" style=\"display: inline-block;\">&nbsp;</ins><ins class=\"jstree-icon\">&nbsp;</ins>" + objArray.data + "</a> ";
-			}
+		liElement.innerHTML = "<ins class=\"jstree-icon\">&nbsp;</ins><a href=\"#\" class=\"\"><ins class=\"jstree-checkbox\" style=\"display: inline-block;\">&nbsp;</ins><ins class=\"jstree-icon\">&nbsp;</ins>" + objArray.data + "</a> ";
 		}else{
 		liElement.innerHTML = "<ins class=\"jstree-icon\">&nbsp;</ins><a href=\"#\" class=\"\"><ins class=\"jstree-checkbox\" style=\"display: none;\">&nbsp;</ins><ins class=\"jstree-icon\">&nbsp;</ins>" + objArray.data + "</a> ";
 		}
