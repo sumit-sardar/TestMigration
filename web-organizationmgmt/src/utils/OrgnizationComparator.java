e01ca0c8df463af8791abab9e76c645d9768439f
ddutta <>
2014-04-25 07:31:28 +0000

    Fix for Production defect 77225. Change in organization comparator parameter from orgCategoryId to orgCategoryLevel

--- web-organizationmgmt/src/utils/OrgnizationComparator.java
+++ web-organizationmgmt/src/utils/OrgnizationComparator.java
@@ -7,16 +7,14 @@ public class OrgnizationComparator implements Comparator<Organization> {
 	public int compare(Organization org1, Organization org2) {
 		if(org1 == null || org2 == null) {
 			return 0;
-		} else if (org1.getOrgCategoryId() == null || org2.getOrgCategoryId() == null ) {
+		} else if (org1.getOrgCategoryLevel() == null || org2.getOrgCategoryLevel() == null ) {
 			return 0;
-		} else if (org1.getOrgCategoryId() > org2.getOrgCategoryId() ) {
+		} else if (org1.getOrgCategoryLevel() > org2.getOrgCategoryLevel() ) {
 			return 1;
-		} else if (org1.getOrgCategoryId() < org2.getOrgCategoryId() ) {
+		} else if (org1.getOrgCategoryLevel() < org2.getOrgCategoryLevel() ) {
 			return -1;
 		}  else {
 			return 0;
 		}
-		
 	}
-
 }
