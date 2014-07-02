650537ba5e5eb36263042d3da3af8851116e2e50
smalligairaj <>
2014-07-02 17:58:41 +0000

    LTITest is for QA purpose only. It simulates Engrade client request

---
+++ LTITest/WebContent/index.jsp
@@ -0,0 +1,67 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>LTI authentication test client</title>
+<script type="text/javascript" src="<%=request.getContextPath()%>/resources/js/jquery-1.6.2.min.js"></script>
+<script>
+function signRequest()
+{
+
+alert("Values sent to server..."+$("#ltiLaunchForm").serialize());
+$.ajax({
+  type: "POST",
+  url: "LTISignServlet",
+  data: $("#ltiLaunchForm").serialize()
+})
+  .done(function( sign ) {
+    alert( "Oauth signature " + sign );
+    $("#oauth_signature").val(sign);
+  });
+}
+function test()
+{
+alert("test");
+}
+</script>
+</head>
+<body><h1>LTI authentication test client</h1><br/>
+<h2>Only  consumer key, user id and signature can be modified.</h2>
+<form action="http://localhost:7001/SessionWeb/LTIAuthentication" name="ltiLaunchForm" id="ltiLaunchForm" method="post" encType="application/x-www-form-urlencoded" name="ltiLaunchForm" id="ltiLaunchForm" method="post" encType="application/x-www-form-urlencoded">
+<table><tr><td>OAuth Version</td><td>
+<input type="text" name="oauth_version" value="1.0" readonly="readonly"/></td>
+</tr><tr>
+<td>OAuth once</td><td>
+<input type="text" name="oauth_nonce" value="c8350c0e47782d16d2fa48b2090c1d8f" readonly="readonly"/></td>
+</tr><tr>
+<td>oauth_timestamp</td><td>
+<input type="text" name="oauth_timestamp" value="1251600739" readonly="readonly"/></td>
+</tr><tr>
+<td><b>oauth consumer key*</b></td><td>
+<input type="text" name="oauth_consumer_key" value="14719" /></td></tr>
+<tr>
+<td><b>user id*</b></td><td>
+<input type="text" name="user_id" value="221483"/></td>
+</tr><tr>
+<td>Roles</td><td>
+<input type="text" name="roles" value="Instructor" readonly="readonly"/></td></tr><tr>
+<td>Full Name</td><td>
+<input type="text" name="lis_person_name_full" value="Jane Q. Public"  readonly="readonly"/></td></tr><tr>
+<td>LTI Version</td><td>
+<input type="text" name="lti_version" value="LTI-1p0" readonly="readonly"/></td></tr><tr>
+<td>LTI message type</td><td>
+<input type="text" name="lti_message_type" value="basic-lti-launch-request" readonly="readonly"/></td></tr><tr>
+<td>OAuth signature method</td><td>
+<input type="text" name="oauth_signature_method" value="HMAC-SHA1" readonly="readonly"/></td></tr><tr>
+<td>Callback page</td><td>
+<input type="text" name="oauth_callback" value="about:blank" readonly="readonly"/></td></tr><tr>
+<td>
+OAuth Signature</td><td>
+<input type="text" name="oauth_signature" id="oauth_signature" value="e5b60db1ef154bf47243be2c6dfe02dc4b88778a" /></td></tr><tr>
+<td><button onclick="javascript:signRequest()" type="button">Calculate signature</button></td>
+<td  align="center">
+<button >Authenticate</button></td></tr>
+</table>
+</form>
+</body>
+</html>
---
+++ LTITest/src/com/ctb/ltitest/LTIAuthServlet.java
@@ -0,0 +1,91 @@
+package com.ctb.ltitest;
+
+
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
+import java.util.Enumeration;
+import java.util.Map;
+import java.util.TreeMap;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class for Servlet: LTIAuthServlet
+ *
+ */
+ public class LTIAuthServlet extends javax.servlet.http.HttpServlet implements javax.servlet.Servlet {
+   static final long serialVersionUID = 1L;
+   static final String OAUTH_PREFIX = "oauth_";
+   static final String OAUTH_SIGNATURE = "oauth_signature";
+    /* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#HttpServlet()
+	 */
+	public LTIAuthServlet() {
+		super();
+	}   	
+	
+	/* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}  	
+	
+	/* (non-Java-doc)
+	 * @see javax.servlet.http.HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		Map<String,String> oauthMap = new TreeMap<String,String>();
+		//read all oauth_ parameters
+		Map<java.lang.String,java.lang.String[]> reqParams = (Map<java.lang.String,java.lang.String[]>)request.getParameterMap();
+	       for(Map.Entry<String,String[]> param :reqParams.entrySet())
+	       {
+	    	   String key = param.getKey();
+	    	   //ignore if key is null or key == "oauth_signature"
+	    	   if(key==null || key.equals(OAUTH_SIGNATURE))  continue;
+	    	   if(key.startsWith(OAUTH_PREFIX))
+	    	   {
+	    		   String[] values = param.getValue();
+	    		   if(values == null || values.length<= 0){
+	    			   oauthMap.put(key, null);
+	    		   }
+	    		   else
+	    		   {
+	    			   String value = values[0];
+	    			   oauthMap.put(key, value);
+	    		   }
+	    	   }
+	       }
+	       StringBuilder baseString = new StringBuilder("POST");
+	       for(Map.Entry<String,String> oauthParam :oauthMap.entrySet())
+	       {
+	    	   baseString.append(oauthParam.getKey()+"&"+oauthParam.getValue()+"=");
+	       }
+	       if(oauthMap.size()>0 && baseString.length()>1)
+	       {
+	    	   String signString = baseString.substring(0,baseString.length()-1);
+	    	   try {
+				HmacSha1Signature.calculateRFC2104HMAC(signString,"key");
+			} catch (InvalidKeyException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (SignatureException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} catch (NoSuchAlgorithmException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+	       }
+	       else
+	       {//error
+	    	   
+	       }
+	}  
+	}
