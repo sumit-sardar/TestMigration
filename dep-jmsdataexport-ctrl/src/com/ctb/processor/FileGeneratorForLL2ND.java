11ba0014d44f0c7a5069d40495a249b89ac677b4
Sumit Sardar <sumit.sardar@mheducation.com>
2014-10-30 22:31:23 +0000

    Defect :#80888 Description Of Defect : Las Links - Form D - export to eMetric - The values for AEL and Other under Program Participation demographic is not getting populated in the emetric file for Form D. Who Did Code Review : Jaivardhan Mehrotra Impacted Areas: Export files generated through Data Export module for Forms C/D How The Issue Was Solved : The demographic values for Program Participation i.e. AEL and Other are only applicable to Form C and Form D, the same have been enable by adding Form D product type check. Hints To QA For Testing : Login to OAS with top-level LASLinks administrator [which has been assigned Form D] and verify eMetric export files.

--- dep-jmsdataexport-ctrl/src/com/ctb/processor/FileGeneratorForLL2ND.java
+++ dep-jmsdataexport-ctrl/src/com/ctb/processor/FileGeneratorForLL2ND.java
@@ -1176,7 +1176,7 @@ public class FileGeneratorForLL2ND {
 		for (StudentDemographic studentDemo : sd) {
 			if(studentDemo.getCustomerDemographicId()==customerDemographicId) {
 				if (studentDemo.getValueName().startsWith("AEL") 
-						&& (productId == 7501)) {
+						&& ((productId == 7501) || (productId == 7505))) {
 					tfil.setAcademicEngLearner("1");
 				} else if (studentDemo.getValueName().startsWith("ESEA")) {
 					tfil.setEseaTitle1("1");
@@ -1189,7 +1189,7 @@ public class FileGeneratorForLL2ND {
 				} else if (studentDemo.getValueName().startsWith("Migrant")) {
 					tfil.setMigrantEducation("1");
 				} else if (studentDemo.getValueName().startsWith("Other")
-						&& (productId == 7501)) {
+						&& ((productId == 7501) || (productId == 7505))) {
 					tfil.setOtherDemograph("1");
 				}
 			}
