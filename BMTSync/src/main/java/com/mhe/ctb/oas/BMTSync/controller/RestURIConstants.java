ade615c6628c8fb57141937d94c257f93fe075ad
sbasa <>
2014-12-19 00:38:31 +0000

    *** empty log message ***

--- BMTSync/src/main/java/com/mhe/ctb/oas/BMTSync/controller/RestURIConstants.java
+++ BMTSync/src/main/java/com/mhe/ctb/oas/BMTSync/controller/RestURIConstants.java
@@ -4,6 +4,7 @@ public class RestURIConstants {
 	
 	public static final String SERVER_URI = "http://sync-gain-qa-elb.ec2-ctb.com";
 	public static final String POST_STUDENTS = "/api/v1/bmt/student"; 
+	public static final String POST_ASSIGNMENTS = "/api/v1/bmt/assignment";
 
 }
 
