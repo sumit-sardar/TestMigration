9fd0074e5c26f68d97f5c34ed69aba9ecf0555e6
sbasa <>
2014-12-24 15:27:29 +0000

    Test Admin API URI

--- BMTSync/src/main/java/com/mhe/ctb/oas/BMTSync/controller/RestURIConstants.java
+++ BMTSync/src/main/java/com/mhe/ctb/oas/BMTSync/controller/RestURIConstants.java
@@ -5,6 +5,7 @@ public class RestURIConstants {
 	public static final String SERVER_URI = "http://sync-gain-qa-elb.ec2-ctb.com";
 	public static final String POST_STUDENTS = "/api/v1/bmt/student"; 
 	public static final String POST_ASSIGNMENTS = "/api/v1/bmt/assignment";
+	public static final String POST_TESTADMIN = "/api/v1/bmt/testadministration";
 
 }
 
