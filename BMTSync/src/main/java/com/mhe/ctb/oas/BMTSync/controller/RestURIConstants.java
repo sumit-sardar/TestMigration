c7b50fbc791b8600cf509cd0faf2e75ca9ce956b
ktracer <>
2015-01-23 20:39:17 +0000

    Making constructor of RestURIConstants private; it's a class for holding text strings and never needs to be instantiated.

--- BMTSync/src/main/java/com/mhe/ctb/oas/BMTSync/controller/RestURIConstants.java
+++ BMTSync/src/main/java/com/mhe/ctb/oas/BMTSync/controller/RestURIConstants.java
@@ -2,6 +2,8 @@ package com.mhe.ctb.oas.BMTSync.controller;
 
 public class RestURIConstants {
 	
+	private RestURIConstants() {}
+	
 	public static final String SERVER_URI = "http://sync-gain-qa-elb.ec2-ctb.com";
 	public static final String POST_STUDENTS = "/api/v1/bmt/student"; 
 	public static final String POST_ASSIGNMENTS = "/api/v1/bmt/assignment";
