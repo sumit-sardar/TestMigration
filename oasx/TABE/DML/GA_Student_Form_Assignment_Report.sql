3abffb5948ae92ec9166acf67bbc94b2cf12331a
Sumit Sardar <sumit.sardar@mheducation.com>
2015-03-11 07:41:14 +0000

    Defect/Story Number: 	 OAS-2015 Description Of Defect/Story: 	GA Spring '15 - EOG - Nightly Export of Students Using SR Who Did Code Review: 	Kingshuk Chakraborty Code Files Changes: 	/oasx/TABE/DML/GA_Student_Form_Assignment_Report.sql Impacted Areas/Regression Areas: 	No impact to any existing TAS modules. This is a sql query to report relevant data to end users for verification and taking corrective measures as needed. How The Issue Was Solved: 	A sql query is written which will extract report GA EOG customer based on the following criteria - •	Student added to TAS via UI Import or UI Add Student (i.e. not via pre-id file import from CTB) •	Student’s accommodation “Screen Reader” turned ON •	Student’s form is 1T Hints To QA For Testing: 	Schedule a GA Spring '15 - EOG session adding some students with SR accommodation and students fro which SR is not set. Forms will be assigned in round robin manner  among 1T-5T forms. Also, turn on SCREEN_READER accommodation using Edit Student/Bulk Accommodation to assign the students form 3T. Now, run the query to validate the report criteria and content mentioned in the story.
     Developer Comments: This will generate report for GA EOG customers only [‘GA_Customer’ configuration and GA EOG product check is added [product_id = 37]]. The following scenarios should be verified –
    Screen Reader Turned ON	- Test Form -	Shows up in report
    Yes	- 1T -	Yes
    No	- 1T -	No
    Yes	- Not 1T - No
     Also, student created by Informatica Import should not show up in the report.

---
+++ oasx/TABE/DML/GA_Student_Form_Assignment_Report.sql
@@ -0,0 +1,52 @@
+
+select system.org_node_name District,
+       school.org_node_name School,
+       class.org_node_name "Class/Teacher",
+       adm.test_admin_name "Test Session Name",
+       stu.grade Grade,
+       stu.last_name "Last Name",
+       stu.first_name "First Name",
+       stu.ext_pin1 "Student ID",
+       acco.screen_reader "Screen Reader Flag",
+       tr.form_assignment "Test Form",
+       tr.updated_date_time "Last Modified Time"
+  from org_node               class,
+       org_node               grade,
+       org_node               school,
+       org_node               system,
+       org_node_parent        class_parent,
+       org_node_parent        grade_parent,
+       org_node_parent        school_parent,
+       student                stu,
+       student_accommodation  acco,
+       test_roster            tr,
+       test_admin             adm,
+       customer_configuration cc
+ where cc.customer_id = adm.customer_id
+   and adm.test_admin_id = tr.test_admin_id
+   and tr.student_id = stu.student_id
+   and stu.student_id = acco.student_id
+   and tr.org_node_id = class.org_node_id
+   and class.org_node_id = class_parent.org_node_id
+   and class_parent.parent_org_node_id = grade.org_node_id
+   and grade.org_node_id = grade_parent.org_node_id
+   and grade_parent.parent_org_node_id = school.org_node_id
+   and school.org_node_id = school_parent.org_node_id
+   and school_parent.parent_org_node_id = system.org_node_id
+   and stu.activation_status = 'AC'
+   and tr.activation_status = 'AC'
+   and class.activation_status = 'AC'
+   and grade.activation_status = 'AC'
+   and school.activation_status = 'AC'
+   and system.activation_status = 'AC'
+   and stu.created_by <> 1
+   and stu.precode_id is null
+   and acco.screen_reader = 'T'
+   and tr.form_assignment = '1T'
+   and adm.product_id = 37
+   and cc.customer_configuration_name = 'GA_Customer'
+   --and cc.customer_id = 
+order by  system.org_node_name,
+          school.org_node_name,
+          grade.org_node_name,
+          class.org_node_name
