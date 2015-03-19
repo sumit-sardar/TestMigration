f504933cb55124d13a3d25825146de4a34d52dc3
ktracer <>
2015-03-19 21:16:09 +0000

    Javadocs for data source configuration bean.

--- BMTSync/src/main/java/com/mhe/ctb/oas/BMTSync/spring/DataSourceConfiguration.java
+++ BMTSync/src/main/java/com/mhe/ctb/oas/BMTSync/spring/DataSourceConfiguration.java
@@ -12,11 +12,20 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.mhe.ctb.oas.BMTSync.exception.ServerException;
 
+/**
+ * Spring configuration in Java format. Specifically used to create a data source for DAOs that can depend on test versus QA/Prod.
+ * @author kristy_tracer
+ */
 @Configuration
 public class DataSourceConfiguration {
 
     private static final Logger logger = Logger.getLogger(DataSourceConfiguration.class);
     
+    /**
+     * DataSource bean, returns a direct connection if on desktop, or a Weblogic connection if on QA/Prod.
+     * @return DataSource representing a connection to the database.
+     * @throws ServerException If anything goes wrong, blocking starting.
+     */
 	@Bean
 	public DataSource oasBmtSyncDataSource() throws ServerException {
         final String query = "select 1 from dual";
@@ -34,7 +43,6 @@ public class DataSourceConfiguration {
         } catch (Exception e) {
             logger.info("[DataSource] Couldn't lookup oasBmtSyncDataSource or execute test SQL using Weblogic JNDI syntax: " + e.getMessage());
         }
-
      
         // Default to direct database access, only used for desktop testing.
         BasicDataSource remoteDS = new BasicDataSource();
