eca10bd2b4083163cf47e287bebac0d09fa4e276
piyalis <>
2015-05-21 09:10:17 +0000

    ~Defect/Story Number: 	OAS-2463 ~Description Of Defect/Story: 	 LLO 2015 - Change Download Software Page ~Who Did Code Review: 	Somenath Chakroborti ~Code Files Changes: 	/oasx/COMMON-UTILS/IRTFileReadUtility/src/fileUtil/TASCFileUtil.java [Branch : OAS94] ~Impacted Areas/Regression Areas: Table : Resource_type , Product_resource in OAS. ~How The Issue Was Solved: 	New resource have been added in resource_type table. Urls have been added in product_resource table against parent-product-id 70000. ~Hints To QA For Testing: Change URLs in the  product_resource table and check if those are shown in UI. ~Developer Comments: New resource type have been added to support dynamic urls for Laslink 3rd party sofware links.

---
+++ oasx/LASLINK/DML/Change Download Software Page.sql
@@ -0,0 +1,16 @@
+--OAS 2463 Change Download Software Page
+insert into resource_type (resource_type_code , resource_type_desc) values ('JRE_URL','Java SE Runtime Environment URL');
+insert into resource_type (resource_type_code , resource_type_desc) values ('ADOBE_AIR','Adobe AIR Runtime URL');
+insert into resource_type (resource_type_code , resource_type_desc) values ('FLASH_PL','Flash player URL');
+insert into resource_type (resource_type_code , resource_type_desc) values ('FLASH_PGPC','PC Flash Player Plugin content debugger URL');
+insert into resource_type (resource_type_code , resource_type_desc) values ('FLASH_PGMC','Macintosh Flash Player Plugin content debugger URL');
+insert into product_resource (product_id, resource_type_code , resource_uri) 
+values ( 70000 , 'JRE_URL','http://www.oracle.com/technetwork/java/javase/downloads/java-archive-downloads-javase7-521261.html#jre-7u45-oth-JPR' );
+insert into product_resource (product_id, resource_type_code , resource_uri) 
+values ( 70000 , 'ADOBE_AIR','http://helpx.adobe.com/air/kb/archived-air-sdk-version.html' );
+insert into product_resource (product_id, resource_type_code , resource_uri) 
+values ( 70000 , 'FLASH_PL','http://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html#Flash%20Player%20archives' );
+insert into product_resource (product_id, resource_type_code , resource_uri) 
+values ( 70000 , 'FLASH_PGPC','https://www.adobe.com/support/flashplayer/downloads.html' );
+insert into product_resource (product_id, resource_type_code , resource_uri) 
+values ( 70000 , 'FLASH_PGMC','https://www.adobe.com/support/flashplayer/' );
