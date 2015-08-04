d497521988cd6ea532eea33b2d95c7334b6cd088
nupurs <>
2015-08-04 13:58:58 +0000

    Defect/Story Number: OAS-3748 Description Of Defect/Story: LLO - Modify Install Software page to download ActiveX and NPAPI Who Did Code Review: Somenath Chakroborti Impacted Areas/Regression Areas: Install Software Page for Laslinks customer. No functional change has been done. How The Issue Was Solved: Install Software Page was modified for Laslinks as per provided mock-up. Also, new urls are updated inside product_resource table. 'resource_uri' field for 'resource_type_code' ADOBE_AIR, FLASH_PL and JRE_URL are updated in table PRODUCT_RESOURCE Hints To QA For Testing: Verify Install Software Page for Laslinks custtomer as per provided mock-up. Developer Comments:	Layout and text is changed for Install Software Page as per provided mock-up. Also, new urls are updated inside product_resource table.

--- oasx/LASLINK/DML/Change Download Software Page.sql
+++ oasx/LASLINK/DML/Change Download Software Page.sql
@@ -14,3 +14,21 @@ insert into product_resource (product_id, resource_type_code , resource_uri)
 values ( 70000 , 'FLASH_PGPC','https://www.adobe.com/support/flashplayer/downloads.html' );
 insert into product_resource (product_id, resource_type_code , resource_uri) 
 values ( 70000 , 'FLASH_PGMC','https://www.adobe.com/support/flashplayer/' );
+
+
+
+
+  update product_resource p
+     set p.resource_uri = 'https://helpx.adobe.com/flash-player/kb/archived-flash-player-versions.html#Flash%20Player%20archives'
+   where p.product_id = 70000
+     and p.resource_type_code = 'FLASH_PL';
+
+  update product_resource p
+     set p.resource_uri = 'https://helpx.adobe.com/air/kb/archived-air-sdk-version.html#main_Archived_Adobe_AIR_downloads'
+   where p.product_id = 70000
+     and p.resource_type_code = 'ADOBE_AIR';
+
+  update product_resource p
+     set p.resource_uri = 'https://java.com/en/download/'
+   where p.product_id = 70000
+     and p.resource_type_code = 'JRE_URL';
